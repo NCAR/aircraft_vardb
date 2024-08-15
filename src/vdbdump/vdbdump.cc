e038655e013c820e20a333e7bbd31411adc85a4f
Catherine dewerd <cdewerd@ucar.edu>
2024-08-15 09:52:00 -0600

    Refactor vdbdump. Default behavior remains the same, but now attributes can be specified on the command line with -ulc. Adding attributes is now easier, and the new attribute category is accessable via the -c option. Added a header and some formatting differences.

--- src/vdbdump/vdbdump.cc
+++ src/vdbdump/vdbdump.cc
@@ -12,8 +12,11 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1993-2014
 */
 
 #include <iostream>
-#include <cstdio>
-#include <cstring>
+//#include <cstdio> 
+//#include <cstring> // removal changes nothing 
+#include <vector>
+#include <unistd.h> //for getopt 
+#include <iomanip> // for setw
 
 #include <raf/vardb.hh>
 #include <raf/VarDBConverter.hh>
@@ -21,64 +24,115 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1993-2014
 bool tracker = false;
 
 /* -------------------------------------------------------------------- */
+void print_usage() {
+    std::cout << "Usage: vdbdump [options] [proj_num | VarDB_filename]\n"
+              << "Options:\n"
+              << "  -h            Display this message\n"
+              << "  -a            Disabled legacy: stream without newlines, different formatting \n"
+              << "  -c            Category\n"
+              << "  -u            Units\n"
+              << "  -l            Long_name\n"; 
+}
+/* -------------------------------------------------------------------- */
 int
 main(int argc, char *argv[])
 {
-  int i = 1;
+  //int i = 1;
+  char opt;
+  std::vector<std::string> attribute_names = {};
+  std::vector<int> width = {};
+  std::vector<int> substring_length = {};
+  bool user_format_specified = false;
 
   if (argc < 2)
   {
-    std::cerr << "Usage: vdbdump [-a] [proj_num | VarDB_filename]\n";
+    print_usage();
     return(1);
   }
 
-  if (strcmp(argv[i], "-a") == 0)
+
+  while ((opt = getopt(argc, argv, "aulch"))!= -1){
+    switch (opt){
+      case 'a':
+        //tracker = true;
+        //i = 1;
+        // uncertain what this is for
+        // breaking to find use cases
+        std::cout << "Unknown use for -a: please revise"<< std::endl;
+        return(1);
+        break;
+      case 'c':
+        width.push_back(12);
+        substring_length.push_back(12);
+        attribute_names.push_back("category");
+        user_format_specified = true;
+        break;
+      case 'u':
+        width.push_back(12);
+        substring_length.push_back(12);
+        attribute_names.push_back("units");
+        user_format_specified = true;
+        break;
+      case 'l':
+        width.push_back(50);
+        substring_length.push_back(50);
+        attribute_names.push_back("long_name");
+        user_format_specified = true;
+      case 'h':
+        std::cout<<"\n h case\n";
+        print_usage();
+      default:
+        std::cout<<"\n default case\n";
+        //print_usage();
+    }
+    if (optind >= argc) {
+      std::cerr << "Expected argument after options\n" << std::endl;
+      return(1); 
+    }
+  
+  }
+
+  if (!user_format_specified)
   {
-     tracker = true;
-     i++;
+    // If no output options default to name, units, long_name
+    width.push_back(12);
+    substring_length.push_back(12);
+    attribute_names.push_back("units");
+    width.push_back(50);
+    substring_length.push_back(50);
+    attribute_names.push_back("long_name");
   }
 
   VDBFile file;
   VarDBConverter vdbc;
-  vdbc.open(&file, argv[i]);
+  std::string substring;
+  //vdbc.open(&file, argv[i]);
+  vdbc.open(&file, argv[optind]);
   if (file.is_valid() == false)
   {
     std::cerr << "vdbdump: Initialize failure.\n";
     return(1);
   }
 
-
-  //printf("num vars=%d\n", file.num_vars());
-
-  for (i = 0; i < file.num_vars(); ++i)
+  // print header
+  std::cout << std::left << std::setw(12) << "name ";
+  for (size_t k = 0; k < attribute_names.size(); ++k) {
+    std::cout << std::left << std::setw(width[k]) << attribute_names[k] << " " ;
+  }
+  std::cout <<std::endl;
+  
+  for (int i = 0; i < file.num_vars(); ++i)
   {
     VDBVar *var = file.get_var(i);
 
-    if (tracker)
-      printf("%-12.12s%-8.8s %-60.60s",
-                    var->name().c_str(),
-                    var->get_attribute("units").c_str(),
-                    var->get_attribute("long_name").c_str());
-    else
-    {
-      printf("%-12.12s %-12.12s %-50.50s",
-                    var->name().c_str(),
-                    var->get_attribute("units").c_str(),
-                    var->get_attribute("long_name").c_str());
-/*
-      if (ntohl(((struct var_v2 *)VarDB)[i].is_analog) == true)
-      {
-        ulong	p;
-
-        printf("%4d sps, ", ntohl(((struct var_v2 *)VarDB)[i].defaultSampleRate));
-        printf("%d %d Vdc",
-		ntohl(((struct var_v2 *)VarDB)[i].voltageRange[0]),
-		ntohl(((struct var_v2 *)VarDB)[i].voltageRange[1]));
-      }
-*/
-      printf("\n");
+    std::cout << std::left << std::setw(12) << var->name();
+    for (size_t j = 0; j < attribute_names.size(); ++j) {
+      substring = var->get_attribute(attribute_names[j]).substr(0,substring_length[j]);    
+      std::cout << std::left 
+		<< std::setw(width[j]) 
+		<< substring << " ";
     }
+    std::cout <<std::endl;
   }
-
   return(0);
 }
